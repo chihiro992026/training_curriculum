25f1356a21d01a7d923117a066a432d7bd74532f
久保田千尋 <kubotachihiro@kubotachihironoMacBook-Pro.local>
2021-10-09 09:06:01 +0900

    issue3

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,20 +2,20 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    get_Week
+    get_week
     @plan = Plan.new
   end
 
   # 予定の保存
   def create
     Plan.create(plan_params)
-    redirect_to action: index:
+    redirect_to action: index
   end
 
   private
 
   def plan_params
-    params.require(calendars:).permit(date:, plan:)
+    params.require(:calendars). permit(:date,:plan)
   end
 
   def get_week
